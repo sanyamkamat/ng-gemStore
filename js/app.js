33f111f6b11f5f8aec96a68c19198f9042afb310
sanyamkamat <sanyamkamat@gmail.com>
2014-05-31 23:03:39 +0530

    making the code cleaner by putting all logic in a new controller such as PanelController

--- js/app.js
+++ js/app.js
@@ -32,4 +32,14 @@
     this.products = gems;
   })
 
+  app.controller('PanelController', function () {
+    this.tab = 1;
+    this.selectTab = function (setTab) {
+      this.tab = setTab;
+    };
+    this.isSelected = function (checkTab) {
+      return this.tab === checkTab;
+    }
+  })
+
 })();
