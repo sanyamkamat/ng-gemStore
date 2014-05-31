d52581101f976a49ce6749ccd2045ac77eb0a2d4
sanyamkamat <sanyamkamat@gmail.com>
2014-05-31 16:26:49 +0530

    more directive - ng-show ng-hide

--- js/app.js
+++ js/app.js
@@ -7,7 +7,9 @@
   var gem = {
     name: 'Dodecahedron',
     price: 2.95,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid architecto consectetur deserunt dignissimos, dolore, esse hic ipsum laborum molestiae mollitia nulla numquam obcaecati optio perspiciatis quibusdam soluta? Dicta, mollitia!'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid architecto consectetur deserunt dignissimos, dolore, esse hic ipsum laborum molestiae mollitia nulla numquam obcaecati optio perspiciatis quibusdam soluta? Dicta, mollitia!',
+    canPurchase: true,
+    soldOut: false
   }
 
   app.controller("StoreController", function () {
