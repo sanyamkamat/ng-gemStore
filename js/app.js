21960f2227eebc5129c19ea728cc968db139e97c
sanyamkamat <sanyamkamat@gmail.com>
2014-06-01 13:11:25 +0530

    adding the add review for the gems

--- js/app.js
+++ js/app.js
@@ -10,21 +10,57 @@
       price: 2,
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid architecto consectetur deserunt dignissimos, dolore, esse hic ipsum laborum molestiae mollitia nulla numquam obcaecati optio perspiciatis quibusdam soluta? Dicta, mollitia!',
       canPurchase: true,
-      img: "gem-01.gif"
+      img: "gem-01.gif",
+      reviews: [
+        {
+          stars: 5,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product sucks",
+          author: "tim@hater.com"
+        }
+      ]
     },
     {
       name: 'Pentagonal Gem',
       price: 5.95,
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid architecto consectetur deserunt dignissimos, dolore, esse hic ipsum laborum molestiae mollitia nulla numquam obcaecati optio perspiciatis quibusdam soluta? Dicta, mollitia!',
       canPurchase: false,
-      img: "gem-02.gif"
+      img: "gem-02.gif",
+      reviews: [
+        {
+          stars: 4,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 2,
+          body: "This product sucks",
+          author: "tim@hater.com"
+        }
+      ]
     },
     {
       name: 'Dodecahedron',
       price: 2.95,
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid architecto consectetur deserunt dignissimos, dolore, esse hic ipsum laborum molestiae mollitia nulla numquam obcaecati optio perspiciatis quibusdam soluta? Dicta, mollitia!',
       canPurchase: true,
-      img: "gem-03.gif"
+      img: "gem-03.gif",
+      reviews: [
+        {
+          stars: 3,
+          body: "I love this product!",
+          author: "joe@thomas.com"
+        },
+        {
+          stars: 1,
+          body: "This product sucks",
+          author: "tim@hater.com"
+        }
+      ]
     }
   ]
 
